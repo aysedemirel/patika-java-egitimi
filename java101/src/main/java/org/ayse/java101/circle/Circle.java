fd22809564f6de217efb6bce9e9c035956eafbd8
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2022-07-14 00:04:33 +0300

    [NEW] Add circle area and perimeter calculator

---
+++ java101/src/main/java/org/ayse/java101/circle/Circle.java
@@ -0,0 +1,45 @@
+package org.ayse.java101.circle;
+
+import java.util.Scanner;
+
+/**
+ * Java ile yarı çapını kullanıcıdan aldığınız dairenin alanını ve
+ * çevresini hesaplayan programı yazın.
+ * Alan Formülü : π * r * r;
+ * Çevre Formülü : 2 * π * r;
+ * Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
+ * Formül : (𝜋 * (r*r) * 𝛼) / 360*/
+public class Circle {
+private final double radius;
+private final double centerAngle;
+  public Circle(){
+    System.out.println("Please enter radius and center angle of the circle");
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Radius: ");
+    radius=scanner.nextDouble();
+    System.out.print("Center angle: ");
+    centerAngle = scanner.nextDouble();
+  }
+
+  public void calculateArea(){
+    double area = Math.PI*radius*radius;
+    System.out.println("Area: "+area);
+  }
+
+  public void calculatePerimeter(){
+    double perimeter = 2*Math.PI*radius;
+    System.out.println("Perimeter: "+perimeter);
+  }
+
+  public void calculateSliceArea(){
+    double sliceArea = (Math.PI * (radius*radius) * centerAngle) / 360;
+    System.out.println("Slice area: "+sliceArea);
+  }
+
+  public static void main(String[] args) {
+    Circle circle = new Circle();
+    circle.calculateArea();
+    circle.calculatePerimeter();
+    circle.calculateSliceArea();
+  }
+}
