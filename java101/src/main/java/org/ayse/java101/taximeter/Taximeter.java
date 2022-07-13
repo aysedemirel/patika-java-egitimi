e5f8db5db9211fdd6185599ec1bed0c88c234ba0
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2022-07-13 23:54:19 +0300

    [NEW] Add taximeter calculator

---
+++ java101/src/main/java/org/ayse/java101/taximeter/Taximeter.java
@@ -0,0 +1,32 @@
+package org.ayse.java101.taximeter;
+
+import java.util.Scanner;
+
+/**
+ * Java ile gidilen mesafeye (KM) göre taksimetre tutarını ekrana yazdıran programı yazın.
+ *
+ * Taksimetre KM başına 2.20 TL tutmaktadır.
+ * Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
+ * Taksimetre açılış ücreti 10 TL'dir.*/
+public class Taximeter {
+  private static final double PER_KM = 2.20;
+  private static final double START_PRICE = 10.0;
+  private final double distance;
+  public Taximeter(){
+    System.out.print("Please enter the distance(km): ");
+    Scanner scanner = new Scanner(System.in);
+    distance=scanner.nextDouble();
+  }
+
+  public void calculateFee()
+  {
+    double result = START_PRICE + distance*PER_KM;
+    result = (result<20)?20:result;
+    System.out.println("Total fee: "+result);
+  }
+
+  public static void main(String[] args) {
+    Taximeter taximeter = new Taximeter();
+    taximeter.calculateFee();
+  }
+}
