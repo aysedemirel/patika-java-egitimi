44637629f212d48039d43b8d863c41c7b164ff19
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2022-07-25 23:07:07 +0300

    [NEW] Add travel price calculator

---
+++ java101/src/main/java/org/ayse/java101/travel/TravelType.java
@@ -0,0 +1,27 @@
+package org.ayse.java101.travel;
+
+public enum TravelType {
+  ONE_WAY(1),
+  ROUND_TRIP(2),
+  UNKNOWN(3);
+
+  int id;
+
+  TravelType(int type) {
+    id = type;
+  }
+
+  public static TravelType getType(int id) {
+    for (TravelType type : values()
+    ) {
+      if (type.getId() == id) {
+        return type;
+      }
+    }
+    return UNKNOWN;
+  }
+
+  public int getId() {
+    return id;
+  }
+}
