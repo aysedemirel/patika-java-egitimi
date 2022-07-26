dbe15510e86c53332dbc3523a8edc9d768b85341
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2022-07-26 22:05:59 +0300

    [NEW] Add average calculator

---
+++ java101/src/main/java/org/ayse/java101/loop/AverageCalculator.java
@@ -0,0 +1,37 @@
+package org.ayse.java101.loop;
+
+import java.util.Scanner;
+
+/**
+ * Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların
+ * ortalamasını hesaplayan programı yazınız.
+ */
+public class AverageCalculator {
+
+  private final int number;
+
+  public AverageCalculator() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Number: ");
+    number = scanner.nextInt();
+  }
+
+  public static void main(String[] args) {
+    AverageCalculator averageCalculator = new AverageCalculator();
+    averageCalculator.calculate();
+  }
+
+  public void calculate() {
+    double sum = 0;
+    double totalNum = 0;
+    for (int i = 0; i <= number; i++) {
+      if (i % 3 == 0 || i % 4 == 0) {
+        sum += i;
+        totalNum++;
+      }
+    }
+    double average = (totalNum != 0) ? (sum / totalNum) : 0;
+    System.out.println("Average: " + average);
+  }
+
+}
