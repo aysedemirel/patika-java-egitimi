83becce0e79442353e0fb60dd0f592df60d60af3
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2022-08-21 13:51:29 +0300

    [NEW] Add exponent calculator example

---
+++ java101/src/main/java/org/ayse/java101/method/ExponentCalculation.java
@@ -0,0 +1,40 @@
+package org.ayse.java101.method;
+
+import java.util.Scanner;
+
+/**
+ * Java dilinde, taban ve üs değerleri kullanıcıdan alınan üs alma işlemini "Recursive" metot
+ * kullanarak yapan programı yazınız.
+ * <p>
+ * Taban değeri giriniz : 5 Üs değerini giriniz : 3 Sonuç : 125
+ */
+public class ExponentCalculation {
+
+  public ExponentCalculation() {
+    Scanner scanner = new Scanner(System.in);
+    boolean isExit = false;
+    int base;
+    int exponent;
+    do {
+      System.out.print("Enter the base value :");
+      base = scanner.nextInt();
+      System.out.print("Enter the exponent: ");
+      exponent = scanner.nextInt();
+      System.out.println("Result: " + getResult(exponent, base));
+      System.out.println("If you want to exit, please press 0: (or continue with another number)");
+      isExit = scanner.nextInt() == 0;
+    } while (!isExit);
+  }
+
+  public static void main(String[] args) {
+    new ExponentCalculation();
+  }
+
+  private int getResult(int exponent, int base) {
+    if (exponent == 0) {
+      return 1;
+    }
+    return base * getResult(exponent - 1, base);
+  }
+
+}
