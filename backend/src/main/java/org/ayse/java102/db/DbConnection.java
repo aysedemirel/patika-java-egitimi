c622b081e6c2c53fb30fde4d67724339f98d399b
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2023-01-14 22:39:56 +0300

    [NEW] Add select query

--- backend/src/main/java/org/ayse/java102/db/DbConnection.java
+++ backend/src/main/java/org/ayse/java102/db/DbConnection.java
@@ -2,19 +2,25 @@ package org.ayse.java102.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbConnection {
 
-  public static final String DB_URL = "jdbc:postgresql://localhost:49153/demodb";
-  public static final String DB_USERNAME = "postgres";
-  public static final String DB_PASSWORD = "postgrespw";
+  private static final String DB_URL = "jdbc:postgresql://localhost:49153/demodb";
+  private static final String DB_USERNAME = "postgres";
+  private static final String DB_PASSWORD = "postgrespw";
 
   public static void main(String[] args) {
-    Connection conn = null;
+    String sql = "Select * from students;";
 
-    try {
-      conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME,
+        DB_PASSWORD); Statement statement = conn.createStatement()) {
+      ResultSet resultSet = statement.executeQuery(sql);
+      while (resultSet.next()) {
+        System.out.println("Full name: " + resultSet.getString("fullname"));
+      }
     } catch (SQLException ex) {
       System.out.println("SQLException: " + ex.getMessage());
       System.out.println("SQLState: " + ex.getSQLState());
